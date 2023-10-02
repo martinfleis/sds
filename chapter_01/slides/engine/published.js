f3937a8d9755c6bdf8d40255fc46362c7fa0a025
Martin Fleischmann <martin@martinfleischmann.net>
2023-10-02 14:53:28 +0200

    use quarto for presentations

--- chapter_01/slides/engine/published.js
+++
@@ -1,7 +0,0 @@
-
-const appHeight = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-window.addEventListener('resize', appHeight);
-appHeight();
